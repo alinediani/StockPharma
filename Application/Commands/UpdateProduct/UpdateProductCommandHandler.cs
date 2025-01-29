169c671040a01bb9de0d35a0fef587e6acec2141
Aline Diani <alinediani99@gmail.com>
2025-01-29 01:22:21 -0300

    Revert "Correção update"
    This reverts commit 31b902b91eccfc998b86aa87c3082b67258fbbf1.

--- Application/Commands/UpdateProduct/UpdateProductCommandHandler.cs
+++ Application/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -28,29 +28,32 @@ namespace Application.Commands.UpdateProduct
                 throw new InvalidOperationException("Product not found.");
             }
 
-            // Atualiza os campos do produto
+            // Atualiza os campos simples do produto
             product.Name = request.Name;
             product.Description = request.Description;
             product.Price = request.Price;
             product.Amount = request.Amount;
 
-            var currentRawMaterials = product.ProductRawMaterials.ToList(); // Crie uma cópia da lista de materiais-primas
+            // Atualiza os materiais-primas do produto
+            var currentRawMaterials = product.ProductRawMaterials.ToList();
 
-            // Identificar e remover materiais-primas que não estão mais no pedido
+            // Remover materiais-primas que não estão mais presentes
             var rawMaterialsToRemove = currentRawMaterials
                 .Where(current => !request.RawMaterials.Any(updated => updated.RawMaterialId == current.RawMaterialId))
                 .ToList();
 
+            // Deleta os materiais-primas removidos
             foreach (var rawMaterialToRemove in rawMaterialsToRemove)
             {
                 await _productRawMaterialRepository.DeleteProductRawMaterialAsync(product.Id, rawMaterialToRemove.RawMaterialId);
             }
 
-            // Identificar e adicionar novos materiais-primas
+            // Identifica novos materiais-primas a serem adicionados
             var rawMaterialsToAdd = request.RawMaterials
                 .Where(updated => !currentRawMaterials.Any(current => current.RawMaterialId == updated.RawMaterialId))
                 .ToList();
 
+            // Adiciona os novos materiais-primas
             foreach (var material in rawMaterialsToAdd)
             {
                 var newProductRawMaterial = new ProductRawMaterialEntity
@@ -68,8 +71,6 @@ namespace Application.Commands.UpdateProduct
             return Unit.Value;
         }
     }
-
-}
 
 
 
